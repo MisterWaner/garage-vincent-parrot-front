ee0fd8fea0f1aaddf2e9bfdd33437078162980a2
Erwan Soulas <erwan.soulas@gmail.com>
2023-08-02 18:29:57 +0200

    make layout of connexion page

--- src/pages/Connexion/Connexion.jsx
+++ src/pages/Connexion/Connexion.jsx
@@ -1,21 +1,50 @@
+import { Link } from "react-router-dom";
+
 const Connexion = () => {
     return (
         <>
-            <main className="max-w-[1400px] mx-auto mt-[160px] p-5 bg-red-02 text-white">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-                aut? Aperiam voluptatum quod ea iusto fugiat ut libero beatae
-                quisquam aliquid cumque maxime soluta, voluptatem aspernatur
-                autem atque accusantium rerum provident debitis itaque est a!
-                Possimus praesentium, reiciendis dignissimos vitae, aperiam illo
-                corrupti fugiat obcaecati sapiente dolore, minima adipisci fuga
-                ipsum totam nam harum doloribus inventore quasi voluptatibus
-                perspiciatis labore sed nostrum mollitia. Doloribus excepturi
-                debitis totam id dolore officiis eum ipsum, hic modi eaque quis
-                voluptatibus aliquid reprehenderit ullam assumenda. Corporis
-                inventore perferendis sint, maiores expedita ducimus! Amet nam
-                cum accusantium? Nostrum deserunt impedit molestias repellendus
-                exercitationem veniam ipsa pariatur eaque ipsam doloribus
-                
+            <main className="max-w-[1400px] mx-auto mt-[160px] p-5 text-white">
+                <h1 className="text-center text-3xl text-yellow-02 underline mb-6 sm:text-5xl decoration-red-02">
+                    Connexion
+                </h1>
+                <section className="w-1/2 mx-auto h-full mt-6 mb-12 lg:w-[500px]">
+                    <form
+                        action=""
+                        className="w-full h-full flex flex-col items-center"
+                    >
+                        <div className="flex flex-col mb-4 w-full">
+                            <label htmlFor="">E-mail</label>
+                            <input
+                                type="email"
+                                className="bg-yellow-02 rounded-sm text-black-02 p-2"
+                            />
+                        </div>
+                        <div className="flex flex-col mb-4 w-full">
+                            <label htmlFor="">Mot de passe</label>
+                            <input
+                                type="password"
+                                className="bg-yellow-02 rounded-sm text-black-02 p-2"
+                            />
+                        </div>
+                        <div className="flex justify-end w-full mt-4">
+                            <button
+                                type="submit"
+                                className="inline-flex h-10 items-center justify-center gap-2 justify-self-center whitespace-nowrap rounded px-5 text-sm font-bold tracking-wide text-red-02 bg-yellow-02 transition duration-300 hover:bg-red-02 hover:text-yellow-02 focus-visible:outline-none disabled:cursor-not-allowed disabled:text-emerald-300 disabled:shadow-none disabled:hover:bg-transparent"
+                            >
+                                Se connecter
+                            </button>
+                        </div>
+                    </form>
+                    <p className="text-center text-sm mt-4">
+                        Mot de passe oublié ?{" "}
+                        <Link
+                            className="no-underline text-yellow-02 font-bold hover:underline hover:decoration-red-02"
+                            to="/inscription"
+                        >
+                            cliquez-ici
+                        </Link>
+                    </p>
+                </section>
             </main>
         </>
     );
