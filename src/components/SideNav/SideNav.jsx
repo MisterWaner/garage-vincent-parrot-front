5039e7e243dce16b247b4a61232097dff351a118
Erwan Soulas <erwan.soulas@gmail.com>
2023-08-10 18:29:39 +0200

    finish sidenav for admin

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -55,28 +55,41 @@ const SideNav = () => {
     };
 
     return (
-        <aside className="h-screen min-h-full bg-red-02 fixed top-0 left-0 z-40 text-yellow-02">
-            <nav className="flex flex-col h-full w-60">
+        <aside className="h-screen min-h-full  fixed top-0 left-0 z-40 text-yellow-02">
+            <nav
+                className={`flex flex-col h-full absolute transition-all duration-300 ease-in w-60 border border-r-yellow-02 ${
+                    open ? "left-0 bg-red-02/60 " : "left-[-180px] bg-red-02"
+                }`}
+            >
                 <button
                     onClick={toggleSideNav}
-                    className="absolute top-6 right-6 border-0 icone-toggle bg-transparent p-1 w-9 h-9"
+                    className="absolute z-40 top-2 right-4 border-0 icone-toggle bg-transparent pl-2 w-9 h-9 hover:text-white duration-300"
                 >
                     {open ? <XMarkIcon /> : <Bars3Icon />}
                 </button>
-                <ul className="w-full h-full absolute transition-all duration-300 ease-in flex flex-col items-center justify-around px-4 py-5 space-y-5 ">
+                <ul
+                    className={` w-full h-full absolute  flex flex-col items-center justify-around px-4 py-5 space-y-5 `}
+                >
                     <Link
                         onClick={handleSignOut}
-                        className="flex items-center justify-between w-full gap-4 text-lg lg-text-xl hover:text-white duration-300"
+                        className="flex items-center justify-between w-full gap-4 text-lg lg-text-xl hover:text-white duration-300 mt-6"
                     >
                         Déconnexion
-                        <ArrowRightOnRectangleIcon className="w-9" />
+                        <ArrowRightOnRectangleIcon className="w-9 pl-2" />
                     </Link>
                     {Links.map((item) => (
-                        <li className="flex items-center justify-between w-full gap-4" key={item.name}>
-                            <Link to={item.link} onClick={toggleSideNav}>
+                        <li
+                            className="flex items-center justify-between w-full gap-4 text-lg lg-text-xl hover:text-white duration-300"
+                            key={item.name}
+                        >
+                            <Link
+                                className="flex items-center justify-between gap-4 w-full"
+                                to={item.link}
+                                onClick={toggleSideNav}
+                            >
                                 {item.name}
+                                <div className="w-9 pl-2">{item.icon}</div>
                             </Link>
-                            <div className="w-9">{item.icon}</div>
                         </li>
                     ))}
                 </ul>
--- src/pages/Auth/Admin/Dashboard/Dashboard.jsx
+++ src/pages/Auth/Admin/Dashboard/Dashboard.jsx
@@ -1,8 +1,10 @@
-
 const Dashboard = () => {
-  return (
-    <div className="mt-[160px] text-white">Dashboard</div>
-  )
-}
+    return (
+        <main className="ml-[80px] text-white">
+            <h1 className="text-center text-3xl text-yellow-02 underline my-6 sm:text-5xl decoration-red-02">Dashboard</h1>
+            <section>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam rerum, placeat numquam laborum aspernatur laudantium, est modi harum soluta laboriosam fugiat maxime explicabo odit itaque repellat, et reprehenderit? Natus, consequuntur? Tempora qui harum ex maiores animi cumque, magni facere, veniam amet debitis enim. Dolorem placeat dicta exercitationem, vero repellendus mollitia.</section>
+        </main>
+    );
+};
 
-export default Dashboard
+export default Dashboard;
