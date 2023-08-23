d227ad70ca0d6756cd566dbf31dc9544b54ed030
Erwan Soulas <erwan.soulas@gmail.com>
2023-08-23 22:37:57 +0200

    ended first part of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,86 @@
 import Planning from "../Planning/Planning";
+import {
+    FaFacebook as FacebookIcon,
+    FaInstagram as InstagramIcon,
+    FaLinkedin as FaLinkedin,
+} from "react-icons/fa6";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 const Footer = () => {
+    const emailAddress = "garage-vincent.parrot@example.com";
+    const phoneNumber = "+33612345678";
+
+    const handleSendEmail = () => {
+        window.location.href = `mailto:${emailAddress}`;
+    };
+
+    const handleDialNumber = () => {
+        window.location.href = `tel:${phoneNumber}`;
+    }
+
+
     return (
         <footer className="sticky top-[100vh]">
             <div className="bg-yellow-02">
-                <div className="grid grid-cols-3">
-                    <div>Footer 1</div>
+                <div className="grid lg:grid-cols-3">
+                    <div className="p-4 lg:my-auto">
+                        <p className="font-bold italic text-center">
+                            Suivez-nous sur les réseaux sociaux :
+                        </p>
+                        <div className="flex gap-4 justify-center mt-8">
+                            <Link
+                                to="https://www.facebook.com/"
+                                target="_blank"
+                            >
+                                <FacebookIcon size={26} />
+                            </Link>
+                            <Link
+                                to="https://www.instagram.com/"
+                                target="_blank"
+                            >
+                                <InstagramIcon size={26} />
+                            </Link>
+                            <Link
+                                to="https://www.linkedin.com/"
+                                target="_blank"
+                            >
+                                <FaLinkedin size={26} />
+                            </Link>
+                        </div>
+                    </div>
                     <div className="p-4">
                         <Planning />
                     </div>
-                    <div>Footer</div>
+                    <div className="p-4 lg:my-auto">
+                        <p className="font-bold italic text-center">
+                            Nous contacter :
+                        </p>
+                        <div className="flex flex-col items-center gap-4 justify-center mt-4">
+                            <p>
+                                Via le formulaire de{" "}
+                                <Link
+                                    to="/contact"
+                                    className="text-black-02 hover:underline hover:un hover:decoration-red-02"
+                                >
+                                    Contact
+                                </Link>{" "}
+                            </p>
+                            <p className="flex gap-2">
+                                Par mail :{" "}
+                                <EnvelopeIcon
+                                    className="w-6 hover:cursor-pointer"
+                                    onClick={handleSendEmail}
+                                />
+                            </p>
+                            <p className="flex gap-2">
+                                Par téléphone :
+                                <PhoneIcon
+                                    className="w-6 hover:cursor-pointer"
+                                    onClick={handleDialNumber}
+                                />
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div className="grid grid-cols-2">
                     <div>Footer 2</div>
--- src/components/Planning/Planning.jsx
+++ src/components/Planning/Planning.jsx
@@ -2,30 +2,30 @@ import { planning } from "../TestDatas/PlanningData.js";
 
 const Planning = () => {
     return (
-        <div className="bg-white/50 p-4 rounded-md">
+        <div className="bg-white/50 p-4 rounded-md border border-black-02">
             <table className="w-full">
                 <caption className="font-bold text-black-02">
                     Nos horaires d&apos;ouverture
                 </caption>
                 <thead>
-                    <tr className="border-b-2 border-black-02 border-spacing-2">
-                        <th>Jours</th>
-                        <th>Matin</th>
-                        <th>Après-midi</th>
+                    <tr className="border-b-2 border-black-02 border-spacing-2 w-full">
+                        <th className="w-1/3">Jours</th>
+                        <th className="w-1/3">Matin</th>
+                        <th className="w-1/3">Après-midi</th>
                     </tr>
                 </thead>
                 <tbody className="text-black-02 text-center">
                     {planning.map((day, index) => (
-                        <tr key={index}>
-                            <td>
+                        <tr key={index} className="w-full">
+                            <td className="w-1/3">
                                 {day.day.charAt(0).toUpperCase() +
                                     day.day.slice(1)}
                             </td>
-                            <td>
+                            <td className="w-1/3">
                                 <span>{day.openingMorning + "  "}</span>
                                 <span>{day.closingMorning}</span>
                             </td>
-                            <td>
+                            <td className="w-1/3">
                                 <span>{day.openingAfternoon + "  "}</span>
                                 <span>{day.closingAfternoon}</span>
                             </td>
