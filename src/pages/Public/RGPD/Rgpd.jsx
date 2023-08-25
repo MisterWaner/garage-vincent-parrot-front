a502ea38f9580a739fecb30c37283519e2b74329
Erwan Soulas <erwan.soulas@gmail.com>
2023-08-25 22:04:50 +0200

    ended rgpd page

--- src/pages/Public/RGPD/Rgpd.jsx
+++ src/pages/Public/RGPD/Rgpd.jsx
@@ -11,41 +11,52 @@ const RgpdPage = () => {
                     <i className="font-bold">Garage Vincent Parot</i> afin de :
                     <br />
                     <ul className="ml-12">
-                        <li className="list-disc">Pouvoir vous recontacter en cas de besoin (informations supplementaires, conseil, envoie de devis, prise de rendez-vous...).</li>
-                        <li className="list-disc">Afficher des commentaires si vous avez accepté leur utilisation.</li>
+                        <li className="list-disc">
+                            Pouvoir vous recontacter en cas de besoin
+                            (informations supplementaires, conseil, envoie de
+                            devis, prise de rendez-vous...).
+                        </li>
+                        <li className="list-disc">
+                            Afficher des commentaires si vous avez accepté leur
+                            utilisation.
+                        </li>
                     </ul>
-                    
                 </p>
                 <p className="my-4">
-                    La base légale du traitement est [base légale du
-                    traitement]. Les données collectées seront communiquées aux
-                    seuls destinataires suivants : [destinataires des données].
-                    Les données sont conservées pendant [durée de conservation
-                    des données prévue par le responsable du traitement ou
-                    critères permettant de la déterminer].
+                    Les données collectées seront communiquées aux
+                    seuls destinataires suivants :{" "}
+                    <i className="font-bold">Garage Vincent Parot</i>. Les
+                    données sont conservées pendant 5 ans maximum.
                 </p>
                 <p className="my-4">
                     Vous pouvez accéder aux données vous concernant, les
                     rectifier, demander leur effacement ou exercer votre droit à
                     la limitation du traitement de vos données (en fonction de
-                    la base légale du traitement, mentionner également : Vous
-                    pouvez retirer à tout moment votre consentement au
-                    traitement de vos données ; Vous pouvez également vous
-                    opposer au traitement de vos données ; Vous pouvez également
+                    la base légale du traitement. Vous pouvez retirer à tout moment votre consentement au traitement de vos données. Vous pouvez également vous
+                    opposer au traitement de vos données. Vous pouvez également
                     exercer votre droit à la portabilité de vos données).{" "}
                 </p>
                 <p className="my-4">
-                    Consultez le site <a href="https://www.cnil.fr/fr" target="_blank" rel="noreferrer" className="hover:underline hover:decoration-red-02">cnil.fr</a> pour plus d’informations sur vos
-                    droits.
+                    Consultez le site{" "}
+                    <a
+                        href="https://www.cnil.fr/fr"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="hover:underline hover:decoration-red-02"
+                    >
+                        cnil.fr
+                    </a>{" "}
+                    pour plus d’informations sur vos droits.
                 </p>
                 <p className="my-4">
                     Pour exercer ces droits ou pour toute question sur le
                     traitement de vos données dans ce dispositif, vous pouvez
                     contacter (le cas échéant, notre délégué à la protection des
                     données ou le service chargé de l’exercice de ces droits) :
-                    la direction du garage Vincent Parrot. Si vous estimez, après nous avoir contactés, que vos
-                    droits « Informatique et Libertés » ne sont pas respectés,
-                    vous pouvez adresser une réclamation à la CNIL.
+                    la direction du garage Vincent Parrot. Si vous estimez,
+                    après nous avoir contactés, que vos droits « Informatique et
+                    Libertés » ne sont pas respectés, vous pouvez adresser une
+                    réclamation à la CNIL.
                 </p>
             </section>
         </main>
