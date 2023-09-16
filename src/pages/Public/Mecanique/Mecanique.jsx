c0ca667035ac355fe284da2fca7986b327822f41
Erwan Soulas <erwan.soulas@gmail.com>
2023-09-16 18:40:36 +0200

    ended mecanics and carrosserie pages, wait to see if it needs some animations

--- src/pages/Public/Mecanique/Mecanique.jsx
+++ src/pages/Public/Mecanique/Mecanique.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
 
 const Mecanique = () => {
+
     return (
         <main className="text-white max-w-[1440px] mx-auto mt-[160px] p-5">
             <h1 className="text-center text-2xl text-yellow-02 underline my-6 sm:text-3xl lg:text-5xl decoration-red-02">
                 Mecanique
             </h1>
-            <section className="py-14">
-                <div className="w-full flex justify-start my-6">
-                    <img className="w-full md:w-1/2 rounded-md lg:w-2/6" src="images/mecanique.jpg" alt="" />
+            <section className="py-14 flex flex-col md:flex-row gap-4 md:items-start ">
+                <div className="w-full md:w-1/2 lg:w-1/3 my-2">
+                    <img className="w-full rounded-md" src="images/mecanique.jpg" alt="" />
                 </div>
-                <article className="my-4">
+                <article className="w-full md:w-1/2 lg:w-2/3">
                     <h2 className="text-xl text-yellow-02 underline underline-offset-2 decoration-red-02 lg:text-2xl">
                         Notre Expertise Mécanique
                     </h2>
-                    <p className="mt-2">
+                    <p className="mt-2 text-lg">
                         Avec des années d&apos;expérience dans le domaine de la
                         mécanique automobile, notre équipe de mécaniciens
                         qualifiés est prête à relever tous les défis mécaniques.
@@ -24,15 +25,16 @@ const Mecanique = () => {
                         toujours dans un état optimal de fonctionnement.
                     </p>
                 </article>
-
-                <div className="w-full flex justify-center my-6">
-                    <img className="w-full md:w-1/2 lg:w-2/6" src="images/frein.jpg" alt="" />
+            </section>
+            <section className="py-14 flex flex-col md:flex-row-reverse gap-4 md:items-start ">
+                <div className="w-full md:w-1/2 lg:w-1/3 my-2">
+                    <img className="w-full rounded-md" src="images/frein.jpg" alt="" />
                 </div>
-                <article className="my-4">
+                <article className="w-full md:w-1/2 lg:w-2/3">
                     <h2 className="text-xl text-yellow-02 underline underline-offset-2 decoration-red-02 lg:text-2xl">
                         Qualité et Engagement
                     </h2>
-                    <p className="mt-2">
+                    <p className="mt-2 text-lg">
                         La qualité est notre credo. Nous nous engageons à
                         n&apos;utiliser que des pièces de rechange de haute
                         qualité et les dernières technologies pour réparer et
@@ -41,16 +43,17 @@ const Mecanique = () => {
                         maintenant ses performances à leur meilleur niveau.
                     </p>
                 </article>
-
-                <div className="w-full flex justify-end my-6">
-                    <img className="w-full md:w-1/2 rounded-md lg:w-2/6" src="images/moteur.jpg" alt="" />
+            </section>
+            <section className="py-14 flex flex-col md:flex-row gap-4 md:items-start ">
+                <div className="w-full md:w-1/2 lg:w-1/3 my-2">
+                    <img className="w-full rounded-md" src="images/moteur.jpg" alt="" />
                 </div>
-                <article className="my-4">
+                <article className="w-full md:w-1/2 lg:w-2/3">
                     <h2 className="text-xl text-yellow-02 underline underline-offset-2 decoration-red-02 lg:text-2xl">
                         Service Personnalisé
                     </h2>
-                    <p className="mt-2">
-                        Chez Garage V. Parrot, nous considérons chaque client
+                    <p className="mt-2 text-lg">
+                        Chez <span className="text-yellow-02 font-bold underline underline-offset-2 decoration-red-02">Garage V. Parrot</span>, nous considérons chaque client
                         comme un membre de notre famille automobile. Notre
                         approche personnalisée signifie que nous écoutons
                         attentivement vos besoins, discutons des options et vous
@@ -59,15 +62,16 @@ const Mecanique = () => {
                         absolue.
                     </p>
                 </article>
-
-                <div className="w-full flex justify-center my-6">
-                    <img className="w-full md:w-1/2 rounded-md lg:w-2/6" src="images/image_slider2.jpg" alt="" />
+            </section>
+            <section className="py-14 flex flex-col md:flex-row-reverse gap-4 md:items-start ">
+                <div className="w-full md:w-1/2 lg:w-1/3 my-2">
+                    <img className="w-full rounded-md" src="images/image_slider2.jpg" alt="" />
                 </div>
-                <article>
+                <article className="w-full md:w-1/2 lg:w-2/3">
                     <h2 className="text-xl text-yellow-02 underline underline-offset-2 decoration-red-02 lg:text-2xl">
                         Nous Contacter
                     </h2>
-                    <p className="mt-2">
+                    <p className="mt-2 text-lg">
                         N&apos;hésitez pas à nous{" "}
                         <span className="bg-yellow-02 text-red-02 p-1 rounded-md">
                             <Link to="/contact">contacter</Link>
@@ -77,17 +81,14 @@ const Mecanique = () => {
                         révision, d&apos;une réparation majeure ou de conseils
                         d&apos;entretien, notre équipe est là pour vous. Nous
                         sommes impatients de vous aider à garder votre véhicule
-                        en parfait état de marche. Merci de nous faire confiance
+                        en parfait état de marche.<br/><br/>  Merci de nous faire confiance
                         pour prendre soin de votre précieux moyen de transport.
-                        Chez Garage V. Parrot, la mécanique automobile est notre
+                        Chez <span className="text-yellow-02 font-bold underline underline-offset-2 decoration-red-02">Garage V. Parrot</span>, la mécanique automobile est notre
                         passion et notre promesse de qualité.
                     </p>
                 </article>
-
-                <div className="w-full flex justify-start mt-6">
-                    <img className="w-full md:w-1/2 rounded-md lg:w-2/6" src="images/chevrolet_camaro.jpg" alt="" />
-                </div>
             </section>
+            
         </main>
     );
 };
